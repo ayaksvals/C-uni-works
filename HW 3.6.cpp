efff85e55d0c898b6a218f3737afc8dbd60c4d89
ayaksvals <90251901+ayaksvals@users.noreply.github.com>
2021-11-09 01:54:44 +0100

    CW 4(3.5-3.7)
    Slavska Olesia compmath2.1

---
+++ HW 3.6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main() 
+{
+    cout<<"a=?  b=?  c=? ";
+    float a,b,c;
+    cin>>a>>b>>c;
+    if (abs(a)>=abs(b) and abs(a)>=abs(b)) cout<<"max: "<<a;
+    else if (abs(b)>=abs(a) and abs(b)>=abs(c)) cout<<"max: "<<b;
+        else cout<<"max: "<<c;
+    c
+    if (abs(a)<=abs(b) and abs(a)<=abs(b)) cout<<" min: "<<a;
+    else if (abs(b)<=abs(a) and abs(b)<=abs(c)) cout<<" min: "<<b;
+        else cout<<" min: "<<c;
+    
+    
+}
